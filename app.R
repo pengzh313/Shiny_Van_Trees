9eac2ceeb33b9fdc38bcc00a011c405035806723
Peng Zhang <pengzh313@gmail.com>
2024-01-08 22:23:27 -0800

    draft app script

--- app.R
+++ app.R
@@ -1,51 +1,76 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Vancouver street trees Shiny App
 
+# Load necessary libraries for the app
 library(shiny)
+library(tidyverse)
+library(geojsonio)
+library(leaflet)
 
-# Define UI for application that draws a histogram
+# read dataset and map
+df <- read_delim("data/street_trees_cleaned.csv")
+url_geojson = "https://raw.githubusercontent.com/UBC-MDS/exploratory-data-viz/main/data/local-area-boundary.geojson"
+van_map <- geojsonio::geojson_read(url_geojson, what = "sp")
+
+# define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
+  # Application title
+  titlePanel("Vancouver Street Trees"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(inputId = "genus",
+                     label = "Select the genus",
+                     choices = NULL
+      )
+    ),
+    
+    # Show a map of the distribution of the selected genus 
+    mainPanel(
+      leafletOutput("treemap")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# define a function to compute number of trees in neighborhoods
+count_trees <- function(df, map) {
+  df_count <- count(df, neighbourhood_name)
+  map@data <- left_join(map@data, 
+                        df_count, 
+                        by = c("name" = "neighbourhood_name"))
+  return(map)
+}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+# define server
+server <- function(input, output, session) {
+  updateSelectizeInput(session, 
+                       "genus", 
+                       choices = df$genus_name,
+                       server = TRUE)
+  
+  df_selected <- reactive(
+    filter(df, genus_name == input$genus))
+  
+  map_data <- reactive(count_trees(df_selected(), van_map))
+  
+  output$treemap <- renderLeaflet(
+    leaflet(map_data()) |>
+      addTiles() |>
+      addPolygons(
+        fillColor = ~colorBin("YlOrRd", domain = map_data()$n)(n),
+        weight = 2,
+        opacity = 1,
+        color = "blue",
+        dashArray = "3",
+        fillOpacity = 0.7) |>
+      addLegend(pal = colorBin("YlOrRd", domain = map_data()$n), 
+                values = ~n, 
+                opacity = 0.7, 
+                title = "Number of trees", 
+                position = "bottomright")
+  )
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
